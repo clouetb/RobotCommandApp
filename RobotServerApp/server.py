c68f68ff6450d294e8bf6e558f1c7adfee50044b
clouetb <benoit.clouet@gmail.com>
2015-11-30 01:10:19 +0100

    I may have forgotten this...

--- RobotServerApp/server.py
+++ RobotServerApp/server.py
@@ -6,21 +6,61 @@ import tornado.httpserver
 import json
 import logging
 import os
+import signal
+import miniupnpc
 import sys
-
 from tornado.options import define, options, parse_command_line
 
+# Current dir
 dirname = os.path.dirname(__file__)
-define("port", default=8888, help="run on the given port", type=int)
-ssl_stuff_dir = os.path.join(dirname, "ssl_stuff")
 
+# Listen on given port
+define("port", default=8888, help="run on the given port", type=int)
+
+multicastif = None
+minissdpdsocket = None
+discoverdelay = 200
+localport = 0
+port_forwarding = None
+
+# Get private key and self-signed certificate
+ssl_stuff_dir = os.path.join(dirname, "ssl_stuff")
 ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 ssl_ctx.load_cert_chain(os.path.join(ssl_stuff_dir, "server.crt"),
                         os.path.join(ssl_stuff_dir, "server.key"))
 
 
-class WebSocketHandler(tornado.websocket.WebSocketHandler):
+def enable_port_forwarding():
+    global port_forwarding
+    try:
+        # Create UPnP client
+        port_forwarding = miniupnpc.UPnP(multicastif, minissdpdsocket, discoverdelay, localport)
+        # Discover internet gateway device
+        log.info("Discovering : %s detected", port_forwarding.discover())
+        # Select internet gateway
+        port_forwarding.selectigd()
+        # Enabling port forwarding
+        log.info("Setting up HTTP port mapping : %s", port_forwarding.addportmapping(8888, "TCP",
+                                                                                     port_forwarding.lanaddr, 8888,
+                                                                                     "Robot-Pi server listener", ""))
+        log.info("Setting up TURN server port mapping : %s", port_forwarding.addportmapping(9999, "TCP",
+                                                                                            port_forwarding.lanaddr,
+                                                                                            9999,
+                                                                                            "Turn server listener", ""))
+    except Exception, e:
+        log.error("Exception : %s", e)
 
+
+def disable_port_forwarding():
+    global port_forwarding
+    try:
+        log.info("Stopping HTTP port mapping : %s", port_forwarding.deleteportmapping(8888, "TCP"))
+        log.info("Stopping TURN server port mapping : %s", port_forwarding.deleteportmapping(9999, "TCP"))
+    except Exception, e:
+        log.error("Exception : %s", e)
+
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def ping(self, value):
         log.debug("Received a ping responding")
         self.write_message("Pong")
@@ -34,6 +74,7 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
         "speed": set_speed
     }
 
+    # Needed for making secure websockets to work
     def check_origin(self, origin):
         return True
 
@@ -42,8 +83,11 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
         log.debug("Client received a message : %s", message)
+        # Parse received json message
         actions = json.loads(message)
+        # loop through messages payloads
         for k in actions.keys():
+            # Call method with message verb
             self.message_types[k](self, actions[k])
 
     def on_close(self):
@@ -51,24 +95,35 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
 
 app = tornado.web.Application([
-        (r'/socket', WebSocketHandler),
-        #(r'/', IndexHandler),
-        (r"/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(dirname,"templates")})
-    ], debug=True)
+    (r'/socket', WebSocketHandler),
+    # (r'/', IndexHandler),
+    (r"/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(dirname, "templates")})
+], debug=True)
+
+
+def set_exit_handler(func):
+    signal.signal(signal.SIGTERM, func)
+
+
+def on_exit(sig, func=None):
+    log.info("Shutting down application")
+    log.debug("Disabling port forwarding")
+    disable_port_forwarding()
+    log.debug("Stopping Tornado server")
+    tornado.ioloop.IOLoop.instance().stop()
+    log.info("Application stopped")
+    sys.exit(0)
+
+# Create a secure http listener
 http_server = tornado.httpserver.HTTPServer(app, ssl_options=ssl_ctx)
 
-
 if __name__ == '__main__':
-    try:
-        parse_command_line()
-        log = logging.getLogger("tornado.application")
-        log.setLevel(logging.DEBUG)
-        http_server.listen(options.port)
-        log.debug("Starting Tornado server")
-        tornado.ioloop.IOLoop.instance().start()
-    except KeyboardInterrupt:
-        log.info("Shutting down application")
-        log.debug("Stopping Tornado server")
-        tornado.ioloop.IOLoop.instance().stop()
-        log.info("Application stopped")
-        sys.exit(0)
+    parse_command_line()
+    set_exit_handler(on_exit)
+    log = logging.getLogger("tornado.application")
+    log.setLevel(logging.DEBUG)
+    http_server.listen(options.port)
+    log.debug("Enabling port forwarding")
+    enable_port_forwarding()
+    log.info("Starting Tornado server")
+    tornado.ioloop.IOLoop.instance().start()
--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -7,26 +7,56 @@ APP_ROOT = os.path.join(REMOTE_DIR, "RobotServerApp")
 
 env.hosts = ["pi@robot-pi.local"]
 
+
 def deploy():
     put(local_path=LOCAL_DIR, remote_path=REMOTE_DIR)
 
 
-def start():
+def populate_var():
+    for executable in ["robot-pi", "chromium-browser"]:
+        for directory in ["log", "run"]:
+            sudo("mkdir -p /var/%s/%s" % (directory, executable))
+            sudo("chown -R pi:pi /var/%s/%s" % (directory, executable))
+
+
+def start_browser():
+    with cd(APP_ROOT):
+        run("./chromium start")
+
+
+def stop_browser():
+    with cd(APP_ROOT):
+        run("./chromium stop")
+
+
+def refresh_browser():
+    with cd(APP_ROOT):
+        run("./chromium refresh")
+
+
+def start_server():
     with cd(APP_ROOT):
         run("./robot-pi start")
 
 
-def stop():
+def stop_server():
     with cd(APP_ROOT):
         run("./robot-pi stop")
 
 
-def restart():
-    stop()
-    start()
+def restart_server():
+    stop_server()
+    start_server()
 
 
-def log():
+def restart_browser():
+    stop_browser()
+    start_browser()
+
+
+def log_server():
     run("tail -f /var/log/robot-pi/robot-pi.log")
 
 
+def log_browser():
+    run("tail -f /var/log/chromium-browser/chromium-browser.log")

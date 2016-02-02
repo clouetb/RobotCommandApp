cd56bbf907b3eabd9e8352d6332ce47c58e142b4
clouetb <benoit.clouet@gmail.com>
2016-02-02 23:42:25 +0100

    Corrected a stupid bug.

--- RobotServerApp/display_manager.py
+++ RobotServerApp/display_manager.py
@@ -8,7 +8,7 @@ log.setLevel(logging.DEBUG)
 
 
 def wake_up_display():
-    if websocket_handler.SignallingWebSocketHandler.clients.count() == 2:
+    if len(websocket_handler.SignallingWebSocketHandler.clients) == 2:
         cmdline = "xset -display :0 dpms force on"
         args = shlex.split(cmdline)
         log.debug("Waking up display with cmdline : %s", args)
